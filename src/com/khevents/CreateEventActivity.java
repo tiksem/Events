30d6993f82cbfceb5596124cbd209cda3b0cdc79
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2015-06-19 23:01:44 +0300

    CreateEventActivity people count ui

--- src/com/khevents/CreateEventActivity.java
+++ src/com/khevents/CreateEventActivity.java
@@ -2,6 +2,8 @@ package com.khevents;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import com.utilsframework.android.view.NumberPickerButton;
 
 /**
  * Created by CM on 6/19/2015.
@@ -11,5 +13,11 @@ public class CreateEventActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_event_activity);
+
+        NumberPickerButton peopleNumber = (NumberPickerButton) findViewById(R.id.people_number);
+        peopleNumber.setMinValue(2);
+        peopleNumber.setMaxValue(1000);
+        peopleNumber.setValue(2);
+        peopleNumber.setPickerDialogTitle(getString(R.string.people_number_dialog_title));
     }
 }
